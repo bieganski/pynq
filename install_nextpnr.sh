3b4a59e1e801d4f4a4c5f0c891b1e778602f9400
Matuesz <bieganski.m@wp.pl>
2022-09-22 23:08:04 +0200

    prjxray git clone added. bitstream generation finishes successfully!

--- install_nextpnr.sh
+++ install_nextpnr.sh
@@ -35,7 +35,24 @@ if ! [[ -f $TOOLPATH ]]; then
     echo "== ERROR: Could not find $TOOLPATH, althought it should have been installed!"
     exit 1
 else
-    echo "== OK, $TOOLPATH binary was compiled! Add it to your PATH!"
-    echo -n 'export PATH=$PATH:'
+    echo "== OK, $TOOLPATH binary was compiled! Add it to your PATH via following command:"
+    echo -n ' $ export PATH=$PATH:'
     echo `dirname $(realpath $TOOLPATH)`
 fi
+
+# Amaranth's Platform.build() creates `build/build_top.sh` script, that invokes series of 
+# yosys and nextpnr scripts. Some of them assume `prjxray-db` to be cloned inside `nextpnr-xilinx`.
+
+pushd $DIRNAME > /dev/null
+
+PRJXRAY_URL=https://github.com/f4pga/prjxray-db
+PRJXRAY_DIRNAME=`basename $PRJXRAY_URL`
+if ! [ -d $PRJXRAY_DIRNAME ]; then
+    echo "== Downloading prjxray-db, needed for routing.."
+    git clone $PRJXRAY_URL
+else
+    echo "== $PRJXRAY_DIRNAME found inside $DIRNAME! Skipping download.."
+
+fi
+
+popd > /dev/null
